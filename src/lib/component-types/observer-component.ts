be3a69853951055bdedc3490252ce5f9e64138f7
torquescrew <sugto555@gmail.com>
2020-04-27 11:45:25 +1200

    Create ObserverComponent by extending Component. This is so we don't have to maintain 2 separate Component implementations.

--- src/lib/component-types/observer-component.ts
+++ src/lib/component-types/observer-component.ts
@@ -1,46 +1,69 @@
-import {equalProps, ParentTree2, Z, ComponentBase, ZType, SubTree, Tree} from './base'
-import {renderInternal} from '../render'
 import {autorun, IReactionDisposer} from 'mobx'
-import {Props} from './component'
+import {Component} from './component'
 
-export class ObserverComponent<P extends {} = {}> implements ComponentBase {
+// export class ObserverComponent2<P extends {} = {}> implements ComponentBase {
+//   disposers: IReactionDisposer[] = []
+//
+//   type = ZType.custom as const
+//   element: HTMLElement
+//   subtree: Z | null = null
+//   props: Props<P>
+//
+//   constructor(props: P, public parent: ParentTree2, public children: SubTree) {
+//     this.props = props
+//     this.props.children = children
+//
+//     this.element = parent.element
+//   }
+//
+//   render(): Tree | null {
+//     return null
+//   }
+//
+//   update() {
+//     const res = this.render()
+//
+//     if (res !== null) this.subtree = renderInternal(this.parent, res, this.subtree, 0)
+//   }
+//
+//   updateWithNewProps(props: P, children: SubTree): void {
+//     const p = props as Props<P>
+//     p.children = children
+//
+//     if (!equalProps(this.props, p)) {
+//       this.props = p
+//       this.update()
+//     }
+//   }
+//
+//   componentDidMount(): void {
+//     //
+//   }
+//
+//   mount() {
+//     this.disposers.push(
+//       autorun(() => {
+//         this.update()
+//       })
+//     )
+//     this.componentDidMount()
+//   }
+//
+//   remove(): void {
+//     this.subtree?.remove()
+//     this.disposers.forEach((d) => d())
+//   }
+//
+//   // Required by JSX
+//   context: any
+//   refs: any
+//   state: any
+//   setState: any
+//   forceUpdate: any
+// }
+
+export class ObserverComponent<P extends {} = {}> extends Component<P> {
   disposers: IReactionDisposer[] = []
-
-  type = ZType.custom as const
-  element: HTMLElement
-  subtree: Z | null = null
-  props: Props<P>
-
-  constructor(props: P, public parent: ParentTree2, public children: SubTree) {
-    this.props = props
-    this.props.children = children
-
-    this.element = parent.element
-  }
-
-  render(): Tree | null {
-    return null
-  }
-
-  update() {
-    const res = this.render()
-
-    if (res !== null) this.subtree = renderInternal(this.parent, res, this.subtree, 0)
-  }
-
-  updateWithNewProps(props: P, children: SubTree): void {
-    const p = props as Props<P>
-    p.children = children
-
-    if (!equalProps(this.props, p)) {
-      this.props = p
-      this.update()
-    }
-  }
-
-  componentDidMount(): void {
-    //
-  }
 
   mount() {
     this.disposers.push(
@@ -55,11 +78,4 @@ export class ObserverComponent<P extends {} = {}> implements ComponentBase {
     this.subtree?.remove()
     this.disposers.forEach((d) => d())
   }
-
-  // Required by JSX
-  context: any
-  refs: any
-  state: any
-  setState: any
-  forceUpdate: any
 }
--- src/lib/create-element.ts
+++ src/lib/create-element.ts
@@ -11,8 +11,6 @@ declare global {
   }
 }
 
-// export const createElementSlice = (...slice: JSXSlice): JSXSlice => slice
-
 export function createElement(
   type: keyof HTMLElementTagNameMap | typeof Component,
   props: Record<string, unknown> | null,
@@ -22,53 +20,5 @@ export function createElement(
     type,
     props,
     children: (children.length === 1 ? children[0] : children) as SubTree,
-  }
-}
-
-export function createArray2() {
-  if (arguments.length > 2) {
-    const children = [arguments[2]]
-
-    for (let i = 3; i < arguments.length; i++) {
-      children.push(arguments[i])
-    }
-    return [arguments[0], arguments[1], children]
-  }
-  return [arguments[0], arguments[1], []]
-}
-
-// export function createObjectSlice(...slice: JSXSlice) {
-//   // const slice = Array.from(arguments)
-//   return {
-//     type: slice[0],
-//     props: slice[1],
-//     children: slice.slice(2),
-//   }
-// }
-//
-// export function createArrayUnshift(...slice: JSXSlice) {
-//   // const slice = Array.from(arguments)
-//   slice.unshift(0)
-//
-//   return slice
-// }
-
-export function createObject2() {
-  if (arguments.length > 2) {
-    const children = [arguments[2]]
-
-    for (let i = 3; i < arguments.length; i++) {
-      children.push(arguments[i])
-    }
-    return {
-      type: arguments[0],
-      props: arguments[1],
-      children,
-    }
-  }
-  return {
-    type: arguments[0],
-    props: arguments[1],
-    children: [],
   }
 }
