bf5bdbcdea931f0bfb183d22b361b386b0903270
torquescrew <sugto555@gmail.com>
2020-06-30 21:54:36 +1200

    Update dependencies.

--- src/dom-tests/test-scheduling-z.test.tsx
+++ src/dom-tests/test-scheduling-z.test.tsx
@@ -6,7 +6,7 @@ import {val} from '../lib/observables/observable'
 import {ZComponent} from '../lib/observables/z-component'
 
 export function sleep(ms: number) {
-  return new Promise((resolve) => {
+  return new Promise(resolve => {
     setTimeout(() => {
       resolve()
     }, ms)
