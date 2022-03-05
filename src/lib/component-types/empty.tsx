c8a537eca39627340ff9133d627d44960455a8ae
torquescrew <t.suggate@kuracloud.com>
2022-03-05 17:44:58 +1300

    Tidy up.

--- src/lib/component-types/empty.tsx
+++
@@ -1,8 +0,0 @@
-// import {ComponentBase, ZType} from './base'
-//
-// export class Empty implements ComponentBase {
-//   parent: unknown
-//   type = ZType.empty as const
-//
-//   remove(): void {}
-// }
